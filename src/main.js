887b8c796fe59436e4d370b2a5e6c565e976203f
Florian Blumenthal <flo.blumenthal@gmx.de>
2017-03-18 21:41:49 +0100

    habe was gendert

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-
+/* mono war hier */
 module.exports.loop = function () {
 
     for(var name in Memory.creeps) {
